a9b47dac2cc246fabadee459dcb2dd6f63fc8264
payalrathore0603 <payalrathore0603@gmail.com>
2026-03-28 22:34:22 +0530

    style: create post UI

--- src/componet/pages/SocialMedia.tsx
+++ src/componet/pages/SocialMedia.tsx
@@ -1,13 +1,62 @@
+import { useNavigate } from "react-router-dom";
+import type { PostType } from "../../types/postType";
+import Post from "./PostCard";
+
+
 
 export default function SocialMedia() {
+  const navigate=useNavigate();
 
-  
+  const posts = [
+  {
+    id: 1,
+    title: "Trip to Manali 🏔️",
+    description: "Amazing experience in the mountains with friends.",
+    category: "Travel",
+    image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+    likes: 12,
+    isLiked: false,
+    isSaved: false,
+    user: {
+      name: "Payal",
+      avatar: "https://i.pravatar.cc/150?img=1"
+    }
+  },
+  {
+    id: 2,
+    title: "Bike ride in Delhi 🏍️",
+    description: "Evening ride with cool breeze and great roads.",
+    category: "Bike",
+    image: "https://images.unsplash.com/photo-1518655048521-f130df041f66",
+    likes: 8,
+    isLiked: false,
+    isSaved: false,
+    user: {
+      name: "Rahul",
+      avatar: "https://i.pravatar.cc/150?img=2"
+    }
+  }
+];
+
+const handleCreatepost=()=>{
+  navigate("/createPost")
+}
+
   return (
-    <div className='max-w-7xl mx-auto pt-4'>
-      <h1>Soical Media</h1>
-      <div className=''>
+   <div className="relative  min-h-screen">
 
+     <div className='pt-6'>
+        {posts.map((post:PostType)=>(
+          <Post key={post.id} post={post} />
+        ))}
       </div>
-    </div>
+        
+     
+      <div className="w-12 h-12 rounded-full border border-gray-500 flex items-center justify-center bottom-2 right-4 fixed cursor-pointer" >
+        <button onClick={handleCreatepost} className="text-2xl">+</button> 
+      </div>
+
+   </div>
+
   )
 }
---
+++ src/types/postType.ts
@@ -0,0 +1,14 @@
+export interface PostType{
+    id:number,
+    title:string,
+    description:string,
+    category:string,
+    image:string,
+    likes:number,
+    isLiked:boolean,
+    isSaved:boolean,
+    user:{
+        name:string,
+        avatar:string
+    }
+}
